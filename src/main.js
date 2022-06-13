5fbed748ccb4a96a7c90605c7fa4f881cabfe12d
JuliaKirik <juliakirichok9878@gmail.com>
2022-06-13 20:35:36 +0300

    Added

---
+++ src/main.js
@@ -0,0 +1,43 @@
+async function delay(data, ms) {
+    return new Promise(resolve => setTimeout(() => resolve(data), ms));
+}
+
+async function getUserInfo() {
+    return delay({ name: 'Vic', age: 21, id: 1 },1000);
+}
+
+async function getUserAvatar(userInfo) {
+    await delay(
+        userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg',
+        1000
+    );
+}
+
+async function getUserAdditionalInfo(userInfo) {
+    await delay(userInfo.interests = ['sport', 'books'], 1000);
+}
+
+async function getResult() {
+    let userInfo = await getUserInfo();
+    await getUserAvatar(userInfo);
+    await getUserAdditionalInfo(userInfo);
+
+    console.log(userInfo);
+}
+
+getResult();
+
+async function getUser() {
+    return { name: 'Vic', age: 21, id: 1 };
+}
+async function getInfo() {
+    try {
+        let user = await getUser();
+        throw new Error('error');
+    } catch (error) {
+        console.error ("New error")
+    }
+}
+ 
+getInfo();
+
